48aed52ffbfb8b66643a35e1959448ad17bf391f
Noah Bolger <ncbolger@uwaterloo.ca>
2020-02-25 16:55:18 -0500

    Improve efficiency, add comments

---
+++ boltzmann_machine.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def convert_data(spin):
+    """
+    :param spin: either +/-
+    :return: +1 or -1 depending on spin orientation
+    """
+    if spin == "+":
+        new_val = 1
+    elif spin == "-":
+        new_val = -1
+    return new_val
+
+
+def hamiltonian(j_vals, chain):
+    """
+    :param j_vals: J coupler values
+    :param chain: single spin coupler chain
+    :return: Hamiltonian value of coupler chain given J coupler value
+    """
+    return -1*np.sum(
+                   [convert_data(chain[i])*convert_data(chain[(i+1) % len(chain)])*j_vals[i]
+                    for i in range(len(chain))]
+    )
+
+
+def partition_function(j_vals, chains):
+    return np.sum([np.exp(-1*hamiltonian(j_vals, c)) for c in chains])
+
+
+def probability(j_vals, chains, Z):
+    return [np.exp(-1*hamiltonian(j_vals, c)) / Z for c in chains]
+
+
+def expectation_value(probabilities, chains):
+    storage = []
+    for spin in range(4):
+        spin2 = (spin + 1) % 4
+        storage.append(np.sum([convert_data(chains[i][spin])*convert_data(chains[i][spin2])*probabilities[i] for i in range(2**4)]))
+    return storage
+
+
+def gradient(probabilities, chains, j_vals):
+    Z = partition_function(j_vals, chains)
+    model_prob = probability(j_vals, chains, Z)
+    pos_phase = expectation_value(probabilities, chains)
+    negative_phase = expectation_value(model_prob, chains)
+    return np.array(pos_phase) - np.array(negative_phase)
--- main.py
+++ main.py
@@ -1,51 +1,24 @@
+import os
 import sys
-import numpy as np
+import json
 from collections import Counter
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+from boltzmann_machine import *
 
 command_inputs = list(sys.argv)
 input_data = command_inputs[1]
 
-
-def convert_data(spin):
-    if spin == "+":
-        new_val = 1
-    elif spin == "-":
-        new_val = -1
-    return new_val
-
-
-def hamiltonian(j_vals, chain):
-    return -1*np.sum(
-                   [convert_data(chain[i])*convert_data(chain[(i+1) % len(chain)])*j_vals[i]
-                    for i in range(len(chain))]
-    )
-
-
-def partition_function(j_vals, chains):
-    return np.sum([np.exp(-1*hamiltonian(j_vals, c)) for c in chains])
-
-
-def probability(j_vals, chains):
-    return [np.exp(-1*hamiltonian(j_vals, c)) / partition_function(j_vals, chains) for c in chains]
-
-
-def expectation_value(probabilities, chains, index1, index2):
-    return np.sum([convert_data(chains[i][index1])*convert_data(chains[i][index2])*probabilities[i] for i in range(2**4)])
-
-
-def gradient(probabilities, chains, index1, j_vals):
-    index2 = (index1 + 1) % 4
-    pos_phase = expectation_value(probabilities, chains, index1, index2)
-    negative_phase = expectation_value(probability(j_vals, chains), chains, index1, index2)
-
-    return np.array(pos_phase) - np.array(negative_phase)
-
 chains = []
 losses = []
 epochs = []
+
+# initial J (coupler values) set to one
+j_vals = [1, 1, 1, 1]
+num_epochs = 0
+learn_rate = 0.1
+loss = 1
 
 KL_div = nn.KLDivLoss()
 with open(input_data) as data:
@@ -56,28 +29,22 @@ chain_occurences = Counter(chains)
 chain_probs = np.array(list(chain_occurences.values())) / 1000
 unique_chains = np.array(list(chain_occurences.keys()))
 
-# initial J (coupler values) set to one
-j_vals = [1, 1, 1, 1]
-num_epochs = 0
-learn_rate = 0.1
-loss = 1
-
 while num_epochs < 200:
 
-    output_probability = torch.from_numpy(np.log(probability(j_vals, unique_chains)))
-    targets = torch.from_numpy(chain_probs)
+    Z = partition_function(j_vals, unique_chains)
+    output_probs = torch.from_numpy(np.log(probability(j_vals, unique_chains, Z)))
+    actual_probs = torch.from_numpy(chain_probs)
 
     epochs.append(num_epochs)
-    new_loss = KL_div(output_probability, targets)
-    losses.append(new_loss)
+    losses.append(KL_div(output_probs, actual_probs))
 
-    update = [gradient(chain_probs, unique_chains, index1, j_vals) for index1 in range(4)]
+    gradients = gradient(chain_probs, unique_chains, j_vals)
 
-    j_vals += learn_rate*np.array(update)
+    j_vals += learn_rate*np.array(gradients)
     num_epochs += 1
 
     if (num_epochs % 25) == 0:
-        print("Loss: {:.4f}".format(KL_div(output_probability, targets)))
+        print("Loss: {:.4f}".format(KL_div(output_probs, actual_probs)))
 
 print("Training loss: {:.4f}".format(losses[-1]))
 rounded_j_vals = np.round(j_vals, 0)
@@ -89,6 +56,15 @@ output_dict = {
     "(3,0)": rounded_j_vals[3]
 }
 
+if not os.path.exists("output/"):
+    os.makedirs("output/")
+
+with open('output/output.json', 'w') as f:
+    json.dump(output_dict, f)
+
 plt.plot(epochs, losses)
+plt.title("KL Divergence over Training Epochs")
+plt.ylabel("KL Divergence (loss)")
+plt.xlabel("Epochs")
 plt.show()
 plt.savefig("output/KL_div.png")
